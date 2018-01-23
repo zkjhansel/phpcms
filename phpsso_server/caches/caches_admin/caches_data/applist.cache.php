a320f850b35cd356fc3342f1f165f6234a0293c0
zkjhansel <1125014902@qq.com>
2018-01-23 14:11:59 +0800

    文件提交

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://cms.com/',
+    'authkey' => 'xKkFNCDiuZ9rfgwFHtOwVE8EUdcKrglR',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
